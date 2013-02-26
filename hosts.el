5bd3d2abd98860ae95aee1dd2994e9acc043a028
John Olsson <john@cryon.se>
2013-02-26 22:09:35 +0100

    fixed paths and flymake stuff...

--- hosts.el
+++ hosts.el
@@ -1,21 +1,21 @@
 (let ((hostname (downcase (car (split-string system-name "\\.")))))
   (when (string-match hostname "elrond")
-    (setenv "PATH"
-	    (concat
-	     "/Users/john/bin:"
-	     "/usr/bin:"
-	     "/bin:"
-	     "/usr/sbin:"
-	     "/sbin:"
-	     "/usr/local/bin:"
-	     "/usr/local/git/bin:"
-	     "/usr/X11/bin:"
-	     "/opt/local/bin:"
-	     "/opt/local/sbin:"
-	     "/usr/local/share/npm/bin:"
-	     "/Users/john/.cabal/bin"))
+    (let ((dirs '("/Users/john/bin"
+		  "/usr/bin"
+		  "/bin"
+		  "/usr/sbin"
+		  "/sbin"
+		  "/usr/local/bin"
+		  "/usr/local/git/bin"
+		  "/usr/X11/bin"
+		  "/opt/local/bin"
+		  "/opt/local/sbin"
+		  "/usr/local/share/npm/bin"
+		  "/Users/john/.cabal/bin")))
+      (setenv "PATH" (mapconcat 'identity dirs path-separator))
+      (setq exec-path (append exec-path dirs)))
 
     (setenv "GHC_MOD"
 	    "/Users/john/.cabal/share/ghc-mod-1.11.4/")
 
-    (setq haskell-program-name "/usr/local/bin/ghci")))
+    (setq haskell-program-name "ghci")))
