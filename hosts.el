3bede8863b47a636064554de6abfc4ff686b895e
John Olsson <john@cryon.se>
2013-06-03 22:35:24 +0200

    paths on my new computer

--- hosts.el
+++ hosts.el
@@ -18,4 +18,22 @@
     (setenv "GHC_MOD"
 	    "/Users/john/.cabal/share/ghc-mod-1.11.4/")
 
+    (setq haskell-program-name "ghci"))
+
+  (when (string-match hostname "orthanc")
+    (let ((dirs '("/usr/lib/qt4/bin"
+		  "/usr/local/bin"
+		  "/usr/bin"
+		  "/bin"
+		  "/usr/local/sbin"
+		  "/usr/sbin"
+		  "/sbin"
+		  "/usr/bin/core_perl"
+		  "/home/john/.cabal/bin")))
+      (setenv "PATH" (mapconcat 'identity dirs path-separator))
+      (setq exec-path (append exec-path dirs)))
+
+    (setenv "GHC_MOD"
+	    "/home/john/.cabal/share/ghc-mod-2.0.3/")
+
     (setq haskell-program-name "ghci")))
