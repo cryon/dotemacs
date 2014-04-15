f9479196dd70efbc9d4a2b4d2a019d9a410c2c1f
John Olsson <john@cryon.se>
2014-04-15 09:55:32 +0200

    I no longer use this computer.

--- hosts.el
+++ hosts.el
@@ -19,21 +19,3 @@
 	    "/Users/john/.cabal/share/x86_64-osx-ghc-7.6.3/ghc-mod-3.1.6")
 
     (setq haskell-program-name "ghci"))
-
-  (when (string-match hostname "orthanc")
-    (let ((dirs '("/usr/lib/qt4/bin"
-		  "/usr/local/bin"
-		  "/usr/bin"
-		  "/bin"
-		  "/usr/local/sbin"
-		  "/usr/sbin"
-		  "/sbin"
-		  "/usr/bin/core_perl"
-		  "/home/john/.cabal/bin")))
-      (setenv "PATH" (mapconcat 'identity dirs path-separator))
-      (setq exec-path (append exec-path dirs)))
-
-    (setenv "GHC_MOD"
-	    "/home/john/.cabal/share/ghc-mod-2.0.3/")
-
-    (setq haskell-program-name "ghci")))
