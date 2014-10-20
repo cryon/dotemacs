e36a14995e89d69ecef8a61e2aad7e8d49ba9f6c
John Olsson <john@cryon.se>
2014-10-20 19:59:10 +0200

    More mode line customizations.

--- auto/21-modeline.el
+++ auto/21-modeline.el
@@ -3,10 +3,10 @@
  '(
    ;; point position
    (8 "%e "
-      (:eval (propertize "%l:" 'face 'font-lock-comment-face))
+      (:eval (propertize "%l:" 'face 'font-lock-string-face))
       (:eval (propertize "%c"  'face (if (>= (current-column) 80)
                                          'font-lock-warning-face
-                                       'font-lock-comment-face))))
+                                       'font-lock-string-face))))
 
    ;; major modes
    ;; not interested in minor modes
@@ -15,14 +15,23 @@
                 help-echo buffer-file-coding-system)
 
    ;; shortened directory
-   (:propertize (:eval (shorten-directory default-directory 30))
+   (:propertize (:eval (shorten-directory default-directory 35))
                 face font-lock-comment-face)
 
    ;; buffer name
    (:propertize "%b" face font-lock-doc-face)
 
-   ;; left align
-   (:eval (propertize " " 'display '((space :align-to (- right-fringe 36)))))
+   ;; jabber activity - right aligned
+   (:eval
+    (propertize
+     " "
+     'display
+     `(space
+       :align-to (- right 22 ,(string-width jabber-activity-mode-string)))))
+   (:eval jabber-activity-mode-string)
+
+   ;; right align
+   (:eval (propertize " " 'display '(space :align-to (- right 20))))
 
    ;; nyan cat saves the day
    (:propertize "|" face vertical-border)
@@ -43,3 +52,18 @@
 
 (add-hook 'haskell-mode-hook
           (lambda() (setq mode-name "Haskell")))
+
+;; helper function
+;; stolen from: http://amitp.blogspot.se/2011/08/emacs-custom-mode-line.html
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
