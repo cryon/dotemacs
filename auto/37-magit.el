6632c94b83257a749bc93b6fd255d2b93a8b851b
John Olsson <john@cryon.se>
2015-07-04 14:39:19 +0200

    I do not want my diff selections to be surrounded by a  thin line.

--- auto/37-magit.el
+++ auto/37-magit.el
@@ -2,5 +2,6 @@
 
 (load-package 'magit)
 
-(setq magit-status-buffer-switch-function 'switch-to-buffer)
-(setq magit-diff-refine-hunk t)
+(setq magit-status-buffer-switch-function 'switch-to-buffer
+      magit-diff-refine-hunk t
+      magit-diff-show-lines-boundary nil)
