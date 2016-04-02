aaed92c5aff6360aa49f80111f7ecfef6b3fb02b
John Olsson <john@cryon.se>
2016-04-02 22:43:22 +0200

    Lets try Source Code Pro as font.

--- auto/01-os-mac.el
+++ auto/01-os-mac.el
@@ -1,6 +1,7 @@
 
 (when (string-match "darwin" (symbol-name system-type))
-  (set-frame-font "Menlo-13")
+  ;; (set-frame-font "Menlo-13")
+  (set-frame-font "Source Code Pro-15")
 
   (setq mac-option-key-is-meta nil
         mac-command-key-is-meta t
