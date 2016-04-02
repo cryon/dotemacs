1e40f6d9452901827744ac920293d137750fba26
John Olsson <john@cryon.se>
2016-04-02 22:46:25 +0200

    Trying tomorrow dark theme for a while.

--- auto/60-visuals.el
+++ auto/60-visuals.el
@@ -1,13 +1,45 @@
-(if window-system
-    (progn
-      (load-package 'rainbow-mode)
+(load-package 'color-theme-sanityinc-tomorrow)
+(load-theme 'sanityinc-tomorrow-night t)
 
-      ;; (load-package 'powerline)
-      ;; (powerline-default-theme)
+;; color theme overrides
+(let ((bg "#1d1f21")
+      (fg "#c5c8c6")
+      (selection "#373b41")
+      (red "#cc6666")
+      (yellow "#f0c674")
+      (green "#b5bd68"))
 
-      (load-package 'subatomic-theme)
-      ;;(setq subatomic-high-contrast t)
-      (load-theme 'subatomic t))
-  (progn
-    (load-package 'subatomic256-theme)
-    (load-theme 'subatomic256 t)))
+  ;; fringe
+  (set-face-attribute 'fringe nil :background bg)
+  (set-face-attribute 'git-gutter-fr:modified nil :background yellow :foreground yellow)
+  (set-face-attribute 'git-gutter-fr:added nil :background green :foreground green)
+  (set-face-attribute 'git-gutter-fr:deleted nil :background red :foreground red)
+
+  ;; whitespace
+  (set-face-attribute 'trailing-whitespace nil :background bg :foreground red :underline t)
+
+  ;; paren
+  (set-face-attribute 'show-paren-match nil :background bg :foreground green :bold t)
+  (set-face-attribute 'show-paren-mismatch nil :background bg :foreground red :bold t)
+
+  ;; mode line
+  (set-face-attribute 'mode-line nil :box `(:line-width 1 :color ,selection))
+  (set-face-attribute 'modeline-inactive nil :box `(:line-width 1 :color ,selection) :background bg)
+
+  ;; cursor
+  (set-cursor-color fg))
+
+;; extras
+(when window-system
+  (load-package 'rainbow-mode))
+
+
+;; leaving this since I cannot make up my mind
+;; (if window-system
+;;     (progn
+;;       (load-package 'subatomic-theme)
+;;       (setq subatomic-high-contrast
+;;       (load-theme 'subatomic t))
+;;   (progn
+;;     (load-package 'subatomic256-theme)
+;;     (load-theme 'subatomic256 t))))
