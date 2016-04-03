92ce792276c69e3c2fc82b2753bfbda19b5e0061
John Olsson <john@cryon.se>
2016-04-03 11:15:36 +0200

    Enable magit-diff-show-lines-boundary.

--- auto/37-magit.el
+++ auto/37-magit.el
@@ -2,6 +2,6 @@
 
 (load-package 'magit)
 
-(setq magit-status-buffer-switch-function 'switch-to-buffer
-      magit-diff-refine-hunk t
-      magit-diff-show-lines-boundary nil)
+(setq
+ magit-status-buffer-switch-function 'switch-to-buffer
+ magit-diff-refine-hunk t)
