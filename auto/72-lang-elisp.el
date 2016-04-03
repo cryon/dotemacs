5a7f51c761b32317e2de7b1c9291441d260182f8
John Olsson <john@cryon.se>
2016-04-03 11:10:48 +0200

    I don't want rainbow-mode by default any more.

--- auto/72-lang-elisp.el
+++ auto/72-lang-elisp.el
@@ -1,6 +1,3 @@
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()
-	    (setq show-trailing-whitespace 1)
-	    (when window-system
-	      (hl-line-mode)
-	      (rainbow-mode))))
+(add-hook
+ 'emacs-lisp-mode-hook
+ (lambda ()))
