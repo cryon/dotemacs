872c4533f2bc173b6b35696c7f009a95ed881b73
John Olsson <john@cryon.se>
2016-05-17 20:08:36 +0200

    Improve rust setup.

--- auto/74-lang-rust.el
+++ auto/74-lang-rust.el
@@ -1,9 +1,19 @@
 (load-package 'rust-mode)
 (load-package 'racer)
+(load-package 'cargo)
+(load-package 'flycheck-rust)
 
-(setq racer-rust-src-path (getenv "RUST_SRC_PATH"))
+(setq racer-rust-src-path (getenv "RUST_SRC_PATH")
+      company-tooltip-align-annotations t)
 
+(add-hook 'rust-mode-hook #'flycheck-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+(add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 
+;; todo: move to key-bindings.el if I like it
 (global-set-key [C-tab] 'company-complete)
-(setq company-tooltip-align-annotations t)
