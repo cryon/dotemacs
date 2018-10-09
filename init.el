64de031f5c0d718e77628a1084dc9fd7a92836f3
John Olsson <john@cryon.se>
2018-10-09 07:58:47 +0200

    Increase gc cons threshold during init

--- init.el
+++ init.el
@@ -1,29 +1,31 @@
 ;;                        Cryon's Emacs configuration
 ;;                        ---------------------------
 
-;; -- Globals --
-(defvar config-dir (file-name-directory load-file-name))
-(defvar hostname (downcase (car (split-string system-name "\\."))))
-(defvar os (symbol-name system-type))
+(let ((gc-cons-threshold most-positive-fixnum))
 
-(defun load-custom (file)
-  (let ((path (concat config-dir file)))
-    (when (file-exists-p path)
-      (load-file path))))
+  ;; -- Globals --
+  (defvar config-dir (file-name-directory load-file-name))
+  (defvar hostname (downcase (car (split-string system-name "\\."))))
+  (defvar os (symbol-name system-type))
 
-(defun load-directory (dir)
-  (mapc (lambda (f) (load-file (concat (file-name-as-directory dir) f)))
-	(directory-files dir nil "\\.el$")))
+  (defun load-custom (file)
+    (let ((path (concat config-dir file)))
+      (when (file-exists-p path)
+	(load-file path))))
 
-;; -- Load custom configurations --
-(load-custom "general.el")
-(load-custom (concat "platform/" "os-" os ".el"))
-(load-custom (concat "platform/" "host-" hostname ".el"))
-(load-custom "packages.el")
-(load-custom "keys.el")
-(load-directory (concat config-dir "auto"))
+  (defun load-directory (dir)
+    (mapc (lambda (f) (load-file (concat (file-name-as-directory dir) f)))
+	  (directory-files dir nil "\\.el$")))
 
-;; -- Last but not least --
-(server-start)
-(cd "~")
-(eshell)
+  ;; -- Load custom configurations --
+  (load-custom "general.el")
+  (load-custom (concat "platform/" "os-" os ".el"))
+  (load-custom (concat "platform/" "host-" hostname ".el"))
+  (load-custom "packages.el")
+  (load-custom "keys.el")
+  (load-directory (concat config-dir "auto"))
+
+  ;; -- Last but not least --
+  (server-start)
+  (cd "~")
+  (eshell))
