f617018627c6ed3e2831d3a9605c0eb6655b7612
John Olsson <john.olsson.ext@netent.com>
2019-07-09 10:00:54 +0200

    Fast init

--- init.el
+++ init.el
@@ -1,31 +1,47 @@
 ;;                        Cryon's Emacs configuration
 ;;                        ---------------------------
 
-(let ((gc-cons-threshold most-positive-fixnum))
+;; -- Globals --
+(defvar config-dir (file-name-directory load-file-name))
+(defvar hostname (downcase (car (split-string system-name "\\."))))
+(defvar os (symbol-name system-type))
 
-  ;; -- Globals --
-  (defvar config-dir (file-name-directory load-file-name))
-  (defvar hostname (downcase (car (split-string system-name "\\."))))
-  (defvar os (symbol-name system-type))
+(defun cryon--load-custom (file)
+  "Load file relative to the configuration folder"
+  (let ((path (concat config-dir file)))
+    (when (file-exists-p path)
+      (load-file path))))
 
-  (defun load-custom (file)
-    (let ((path (concat config-dir file)))
-      (when (file-exists-p path)
-	(load-file path))))
+(defun cryon--load-directory (dir)
+  "Load all .el files in a directory relative to the configuration folder"
+  (mapc (lambda (f) (load-file (concat (file-name-as-directory dir) f)))
+	(directory-files dir nil "\\.el$")))
 
-  (defun load-directory (dir)
-    (mapc (lambda (f) (load-file (concat (file-name-as-directory dir) f)))
-	  (directory-files dir nil "\\.el$")))
+(defun cryon--fast-init (init-function)
+  "Apply the init function with less overhead"
+  (defvar cryon--file-name-handler-alist file-name-handler-alist)
+  (setq gc-cons-threshold 402653184
+	gc-cons-percentage 0.6
+	file-name-handler-alist nil)
+  (add-hook
+   'emacs-startup-hook
+   (lambda ()
+     (setq gc-cons-threshold 16777216
+	   gc-cons-percentage 0.1
+	   file-name-handler-alist cryon--file-name-handler-alist)))
+  (funcall init-function))
 
-  ;; -- Load custom configurations --
-  (load-custom "general.el")
-  (load-custom (concat "platform/" "os-" os ".el"))
-  (load-custom (concat "platform/" "host-" hostname ".el"))
-  (load-custom "packages.el")
-  (load-custom "keys.el")
-  (load-directory (concat config-dir "auto"))
+(defun cryon--init ()
+  "The actual init function"
+  (cryon--load-custom "general.el")
+  (cryon--load-custom (concat "platform/" "os-" os ".el"))
+  (cryon--load-custom (concat "platform/" "host-" hostname ".el"))
+  (cryon--load-custom "packages.el")
+  (cryon--load-custom "keys.el")
+  (cryon--load-directory (concat config-dir "auto"))
 
-  ;; -- Last but not least --
   (server-start)
   (cd "~")
   (eshell))
+
+(cryon--fast-init 'cryon--init)
