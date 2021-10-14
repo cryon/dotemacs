b1ad1ae94355a7125da6ad198533193cb523258a
John Olsson <johnolsson@spotify.com>
2021-10-14 09:11:47 +0200

    Simplify eshell config

--- auto/eshell.el
+++ auto/eshell.el
@@ -1,50 +1,16 @@
-(use-package xterm-color :ensure t
-  :init
-  (setenv "TERM" "xterm-256color")
-  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-  (setq comint-output-filter-functions
-	(remove 'ansi-color-process-output comint-output-filter-functions)))
-
 (use-package eshell
   :init
   (setq
    eshell-save-history-on-exit t
-
-   ;; make the input history more bash-like
    eshell-hist-ignoredups t
    eshell-cmpl-cycle-completions nil
+   eshell-scroll-to-bottom-on-input 'all))
 
-   ;; scroll to the bottom
-   eshell-scroll-to-bottom-on-output t
-   eshell-scroll-show-maximum-output t
+(use-package esh-mode
+  :config
+  (defun eshell/clear ()
+    (interactive)
+    (eshell/clear-scrollback))
 
-   eshell-path-env (getenv "PATH"))
-
-  :hook
-  ((eshell-pre-command . (lambda () (setq eshell-path-env (getenv "PATH"))))
-   (eshell-mode-hook . (lambda () (setq eshell-path-env (getenv "PATH"))))))
-
-;; clear - clears the buffer
-(defun eshell/clear ()
-  (interactive)
-  (eshell/clear-scrollback))
-
-;; emacs file - opens file
-(defun eshell/emacs (file)
-  (find-file file))
-
-;; magit - runs magit-status in current directory
-(defun eshell/magit ()
-  (call-interactively 'magit-status))
-
-;; simple but effective prompt
-(setq eshell-prompt-regexp "^[^#$\n]*[#$] "
-      eshell-prompt-function
-      (lambda nil
-        (concat
-	 ;;"[" (user-login-name) "@" (system-name) " "
-	 (if (string= (eshell/pwd) (getenv "HOME"))
-	     "~" (eshell/basename (eshell/pwd)))
-	 ;;"]"
-	 " "
-	 (if (= (user-uid) 0) "# " "$ "))))
+  (defun eshell/emacs (file)
+    (find-file file)))
