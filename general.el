2f65e8db1707b23cffdee1c0d22ff7fa70ca4b1a
John Olsson <johnolsson@spotify.com>
2021-10-14 09:56:05 +0200

    Center frame on startup

--- general.el
+++ general.el
@@ -64,3 +64,24 @@
 
 ;; y or n instead of yes or no
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+
+;; from https://christiantietze.de/posts/2021/06/emacs-center-window-single-function/
+(defun my/frame-recenter (&optional frame)
+  "Center FRAME on the screen.
+FRAME can be a frame name, a terminal name, or a frame.
+If FRAME is omitted or nil, use currently selected frame."
+  (interactive)
+  (unless (eq 'maximised (frame-parameter nil 'fullscreen))
+    (let* ((frame (or (and (boundp 'frame) frame) (selected-frame)))
+            (frame-w (frame-pixel-width frame))
+            (frame-h (frame-pixel-height frame))
+            ;; frame-monitor-workarea returns (x y width height) for the monitor
+            (monitor-w (nth 2 (frame-monitor-workarea frame)))
+            (monitor-h (nth 3 (frame-monitor-workarea frame)))
+            (center (list (/ (- monitor-w frame-w) 2)
+                          (/ (- monitor-h frame-h) 2))))
+      (apply 'set-frame-position (flatten-list (list frame center))))))
+
+(add-hook 'after-init-hook #'my/frame-recenter)
+(add-hook 'after-make-frame-functions #'my/frame-recenter)
