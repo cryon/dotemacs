bf4c92ea920b2d268ea80d7e5a186b6b3b7a8ffa
John Olsson <john@cryon.se>
2025-03-12 21:04:16 +0100

    Add folder for automatic loading of elisp files

--- auto/md-timesheet-functions.el
+++ auto/md-timesheet-functions.el
@@ -1,4 +1,4 @@
-(defun cryon/gen-time-sheet (year month)
+(defun md-timesheet/generate (year month)
   "Creates a time sheet for a month formatted as Markdown"
   (interactive
    (list
@@ -6,13 +6,13 @@
     (read-number "Month: " (nth 4 (decode-time (current-time))))))
   (let ((month-name (format-time-string "%B" (encode-time 0 0 0 1 month 2525))))
     (insert "# Time sheet: " month-name " " (number-to-string year) "\n\n"))
-  (--cryon/gen-month-table year month))
+  (--generate-month-table year month))
 
-(defun --cryon/gen-month-table (year month)
+(defun --generate-month-table (year month)
   (let ((total-hours 0)
         (day 1)
         (time (encode-time 0 0 0 1 month year)))
-    (insert "|Date       |Hours|Comment|\n|-----------|-----|--------|\n")
+    (insert "|Date|Hours|Comment|\n|----|-----|-------|\n")
     (while (= (nth 4 (decode-time time)) month)
       (let* ((weekday (nth 6 (decode-time time)))
              (weekend (or (= weekday 0) (= weekday 6)))
@@ -28,7 +28,7 @@
       (previous-line)
       (markdown-table-align))))
 
-(defun cryon/sum-column-above ()
+(defun md-timesheed/sum-rows-above ()
   "Sums the column above and replace the content of the current cell with that sum"
   (interactive)
   (when (not (markdown-table-at-point-p))
@@ -54,54 +54,3 @@
 	(kill-word 1)
 	(insert (number-to-string sum))
 	(markdown-table-align)))))
-
-
-
-
-
-;; (defun cryon/current-cell-content ()
-;;   (save-excursion
-;;     (let (cell-content)
-;;       (re-search-backward "|" nil t)
-;;       (forward-char)
-;;       (when (re-search-forward "\\([^|]+\\)|" nil t)
-;;         (setq cell-content (match-string 1)))
-;;       (string-trim cell-content))))
-
-;; (defun cryon/sum-column-above ()
-;;   ""
-;;   (interactive)
-
-;;   (defun is-delimiter-p (s)
-;;     (string-match-p "^--+$" s))
-
-;;   (save-excursion
-;;     ;; Todo: We're currently assuming that we're in a markdown table
-
-;;     ;; Move to the start of cell
-;;     (re-search-backward "|" nil t)
-;;     (forward-char)
-
-;;     (save-excursion
-;;       ;; If the cell directly above the
-;;       (previous-line)
-;;       (markdown-table
-;;       (let ((col (current-column))
-;; 	    (cell-content (cryon/current-cell-content))
-;; 	    (sum 0))
-
-;; 	(while cell-content
-
-
-
-;;       (save-excursion
-;; 	;; Move upwards until we hit a delimiter, unless it's the row
-;; 	;; right before. Then we just continue until the next delimiter.
-;; 	(while (cell-content
-;; 	  (previous-line)
-;; 	  (move-to-column col)
-;; 	  (setq sum (+ sum (string-to-number (cryon/current-cell-content))))))
-
-;;       (kill-word 1)
-;;       (insert (num-to-string sum))
-;;       (markdown-table-align))))
--- init.el
+++ init.el
@@ -13,10 +13,16 @@
     (when (file-exists-p path)
       (load-file path))))
 
+(defun --load-config-directory (relative-dir)
+  "Load all .el files in a directory relative to the configuration folder"
+  (let ((dir (concat config-dir relative-dir)))
+    (mapc (lambda (f) (load-file (concat (file-name-as-directory dir) f)))
+	  (directory-files dir nil "\\.el$"))))
+
 (--load-config-file "general.el")
-(--load-config-file "functions.el")
 (--load-config-file "packages.el")
 (--load-config-file "keybindings.el")
+(--load-config-directory "auto")
 
 (when (eq system-type 'darwin)
   (--load-config-file "mac-configurations.el"))
