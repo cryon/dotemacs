93ecb4641d903634a6d667edca55acfe496728c5
John Olsson <john@cryon.se>
2025-05-14 17:36:00 +0200

    Org tweaks and org-roam

--- auto/org-mode.el
+++ auto/org-mode.el
@@ -1,25 +1,47 @@
-;; Todo: This is very sloppy copy and paste
-;; mostly from https://github.com/james-stoup/emacs-org-mode-tutorial
-;; needs some cleanup
-
-(use-package org-superstar
-  :ensure t
-  :hook ((org-mode . org-superstar-mode)))
-
 (setq org-return-follows-link   t
       org-hide-emphasis-markers t)
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'org-indent-mode)
+(add-hook 'org-mode-hook 'visual-line-mode)
 
-(let* ((base-font-color     (face-foreground 'default nil 'default))
-       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+(use-package org-superstar
+  :ensure t
+  :hook ((org-mode . org-superstar-mode)))
 
-  (add-hook 'org-mode-hook 'visual-line-mode)
-  (custom-theme-set-faces
-   'user
-   `(org-level-4 ((t (,@headline :height 1.1))))
-   `(org-level-3 ((t (,@headline :height 1.2))))
-   `(org-level-2 ((t (,@headline :height 1.3))))
-   `(org-level-1 ((t (,@headline :height 1.5))))
-   `(org-document-title ((t (,@headline :height 1.6 :underline nil))))))
+(custom-theme-set-faces
+ 'user
+ '(org-level-4 ((t (:weight bold :height 1.0))))
+ '(org-level-3 ((t (:weight bold :height 1.1))))
+ '(org-level-2 ((t (:weight bold :height 1.2))))
+ '(org-level-1 ((t (:weight bold :height 1.3))))
+ '(org-document-title ((t (:weight bold :height 1.5 :underline nil)))))
+
+
+(let ((roam-notes-directory "~/RoamNotes"))
+  (when (file-directory-p roam-notes-directory)
+    (use-package org-roam
+      :ensure t
+      :custom
+      (org-roam-directory roam-notes-directory)
+      (org-roam-complete-everywhere t)
+      (org-roam-capture-templates
+       '(("d" "default" plain
+	  "%?"
+	  :if-new
+	  (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+	  :unnarrowed t)))
+      :bind
+      (("C-c n l" . org-roam-buffer-toggle)
+       ("C-c n f" . org-roam-node-find)
+       ("C-c n i" . org-roam-node-insert)
+       :map org-mode-map
+       ("C-M-i" . completion-at-point)
+       :map org-roam-dailies-map
+       ("Y" . org-roam-dailies-capture-yesterday)
+       ("T" . org-roam-dailies-capture-tomorrow))
+      :bind-keymap
+      ("C-c n d" . org-roam-dailies-map)
+      :config
+      (require 'org-roam-dailies)
+      (org-roam-db-autosync-mode))))
