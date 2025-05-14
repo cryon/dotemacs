9849ba1b4b88d89d1493d30d7f6d8a1af8aea79e
John Olsson <john@cryon.se>
2025-05-14 20:53:47 +0200

    Reverting the use of variables in use-package

--- auto/org-mode.el
+++ auto/org-mode.el
@@ -17,31 +17,28 @@
  '(org-level-1 ((t (:weight bold :height 1.3))))
  '(org-document-title ((t (:weight bold :height 1.5 :underline nil)))))
 
-
-(let ((roam-notes-directory "~/RoamNotes"))
-  (when (file-directory-p roam-notes-directory)
-    (use-package org-roam
-      :ensure t
-      :custom
-      (org-roam-directory roam-notes-directory)
-      (org-roam-complete-everywhere t)
-      (org-roam-capture-templates
-       '(("d" "default" plain
-	  "%?"
-	  :if-new
-	  (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-	  :unnarrowed t)))
-      :bind
-      (("C-c n l" . org-roam-buffer-toggle)
-       ("C-c n f" . org-roam-node-find)
-       ("C-c n i" . org-roam-node-insert)
-       :map org-mode-map
-       ("C-M-i" . completion-at-point)
-       :map org-roam-dailies-map
-       ("Y" . org-roam-dailies-capture-yesterday)
-       ("T" . org-roam-dailies-capture-tomorrow))
-      :bind-keymap
-      ("C-c n d" . org-roam-dailies-map)
-      :config
-      (require 'org-roam-dailies)
-      (org-roam-db-autosync-mode))))
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/RoamNotes")
+  (org-roam-complete-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new
+      (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)))
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n i" . org-roam-node-insert)
+   :map org-mode-map
+   ("C-M-i" . completion-at-point)
+   :map org-roam-dailies-map
+   ("Y" . org-roam-dailies-capture-yesterday)
+   ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies)
+  (org-roam-db-autosync-mode))
